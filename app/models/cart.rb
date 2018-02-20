dfa69c1ed039248531c663f6945caea469540631
dryney <dryney@gmail.com>
2018-02-20 08:49:04 +0300

    fix cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,19 +4,19 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
   private
   # ensure that there are no line items referencing this product
+  def add_product(product_id)
+		current_item = line_items.find_by(product_id: product_id)
+			if current_item
+				current_item.quantity += 1
+			else
+				current_item = line_items.build(product_id: product_id)
+			end
+		current_item
+	end
   def ensure_not_referenced_by_any_line_item
     unless line_items.empty?
       errors.add(:base, 'Line Items present')
       throw :abort
     end
-  end
-  def add_product(product_id)
-    current_item = line_items.find_by(product_id: product_id)
-    if current_item
-      current_item.quantity += 1
-    else
-      current_item = line_items.build(product_id: product_id)
-    end
-    current_item
   end
 end
