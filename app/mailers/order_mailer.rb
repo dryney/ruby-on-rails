adb74e705d4a8fdd11907282235364750c888411
dryney <dryney@gmail.com>
2018-02-20 19:46:20 +0300

    e-mail message

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,28 @@
+class OrderMailer < ApplicationMailer
+default from: '@dryney <dryney@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+  def shipped(order)
+@order = order
+mail to: order.email, subject: 'Food Store Order Shipped'
+end
+end
