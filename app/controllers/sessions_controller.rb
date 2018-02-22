bcaca6802c4b9e640278aefb8a7e25e2c0b8e7a3
dryney <dryney@gmail.com>
2018-02-22 09:16:07 +0300

    administrator can not be deleted

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
-
 class SessionsController < ApplicationController
   skip_before_action :authorize
+
   def new
   end
 
   def create
     user = User.find_by(name: params[:name])
-    if user && user.authenticate(params[:password])
+    if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
