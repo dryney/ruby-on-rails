141f94fadf9e8a7601a2d74b7f530f41257ff66f
dryney <dryney@gmail.com>
2018-02-22 10:46:33 +0300

    dont del products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.delete_all
 # . . .
 Product.create(title: 'Seven Mobile Apps in Seven Weeks',
 description:
