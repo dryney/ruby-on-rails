41708b6dedf06198f3987773b289dd5e64eb6b12
dryney <dryney@gmail.com>
2018-02-22 16:02:45 +0300

    Web release

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
+# encoding: utf-8
 Product.delete_all
+Product.create(title: 'Android™ Notes for Professionals book',
+  description:
+    %{<p>
+      <em>Powerful, Effective, and Efficient Full-Stack Development</em>
+      The Android™ Notes for Professionals book is compiled from Stack Overflow Documentation, the content is written by the beautiful people at Stack Overflow. Text content is released under Creative Commons BY-SA. See credits at the end of this book whom contributed to the various chapters. Images may be copyright of their respective owners unless otherwise specified.
+      Book created for educational purposes and is not affiliated with Android™ group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners
+      </p>},
+  image_url: 'Android.png',
+  price: 45.00)
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
-description:
-%{<p>
-<em>Native Apps, Multiple Platforms</em>
-Answer the question “Can we build this for ALL the devices?” with a
-resounding YES. This book will help you get there with a real-world
-introduction to seven platforms, whether you’re new to mobile or an
-experienced developer needing to expand your options. Plus, you’ll find
-out which cross-platform solution makes the most sense for your needs.
-</p>},
-image_url: '7apps.jpg',
-price: 26.00)
+Product.create(title: 'C# Notes for Professionals book',
+  description:
+    %{<p>
+      <em>Native Apps, Multiple Platforms</em>
+      The C# Notes for Professionals book is compiled from Stack Overflow Documentation, the content is written by the beautiful people at Stack Overflow. Text content is released under Creative Commons BY-SA. See credits at the end of this book whom contributed to the various chapters. Images may be copyright of their respective owners unless otherwise specified.
+      Book created for educational purposes and is not affiliated with C# group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners
+      </p>},
+  image_url: 'CSharp.png',
+  price: 27.00)
 # . . .
+
+Product.create(title: 'CSS Notes for Professionals book',
+  description:
+    %{<p>
+      <em>WThis is gold. Programming Notes for Professionals book</em>
+      The CSS Notes for Professionals book is compiled from Stack Overflow Documentation, the content is written by the beautiful people at Stack Overflow. Text content is released under Creative Commons BY-SA. See credits at the end of this book whom contributed to the various chapters. Images may be copyright of their respective owners unless otherwise specified.
+      Book created for educational purposes and is not affiliated with CSS group(s), company(s) nor Stack Overflow. All trademarks belong to their respective company owners
+      </p>},
+  image_url: 'Css.png',
+  price: 46.00)
+
+  User.delete_all
+  User.create(name: 'admin',
+  password: 'admin')
